4d586189719ed03c49850b623899bdf296cca2a4
cedricbastin <cedricbastin@gmail.com>
2015-03-22 19:47:19 +0100

    working draft

--- src/main/scala/matrix/Matrixes.scala
+++ src/main/scala/matrix/Matrixes.scala
@@ -11,41 +11,38 @@ object Matrixes extends App {
 
   trait MatrixSig extends Signature {
     type Alphabet = Matrix // Input matrices as (rows, columns) we want to find best multiplication sequence
+    type Answer = Cost //MatrixAlgebra
     def single:Alphabet => Answer
     def mult:(Answer, Answer) => Answer
   }
 
-  class MatrixGrammar extends BaseParsers with MatrixSig {
+  trait MatrixGrammar extends BaseParsers with MatrixSig {
     //def tabulate(name:String, inner: => Parser[Answer], alwaysValid:Boolean=false) = new Tabulate(inner,name,alwaysValid)
     //class Tabulate(in: => Parser[Answer], val name:String, val alwaysValid:Boolean=false) extends Parser[Answer]
 
     // Monoid?
+    val simple:Parser[Answer] = ((el map single)) | ((el ~ simple) map {case (x,y) => y})
+//    val chain:Parser[Answer] = (
+//      (el map (single))
+//        | ((chain ~ chain) map {case (a1, a2) => mult(a1, a2)})
+//    )
 
-    val chain:Parser[Answer] = (
-      (el map (single))
-        | ((chain ~ chain) map {case (a1, a2) => println("here"); println(a1+" "+a2); mult(a1, a2)})
-    )
-
-    val input = Array(Matrix(1,2),Matrix(2,3),Matrix(3,4)) //define the input or the alphabet translations?
-    //val axiom=chain
-    type Answer = Matrix//FIXME Cost //new bound and cost of multiplication (in the middle)
-    //val h = minBy[Answer, Int] //FIXME
-    val single = (m: Alphabet) => m//Cost(0, m) //0 cost if no multiplication
-    val mult = (c1: Answer, c2: Answer) => c1//Cost(c1.v + c2.v + c1.m.x*c2.m.x*c2.m.y, Matrix(c1.m.x, c2.m.y))
-
+    //val axiom=chain //FIXME: needed?
   }
 
   trait MatrixAlgebra extends MatrixSig {
-    type Answer = Matrix//FIXME Cost //new bound and cost of multiplication (in the middle)
     //val h = minBy[Answer, Int] //FIXME
-    val single = (m: Alphabet) => m//Cost(0, m) //0 cost if no multiplication
-    val mult = (c1: Answer, c2: Answer) => c1//Cost(c1.v + c2.v + c1.m.x*c2.m.x*c2.m.y, Matrix(c1.m.x, c2.m.y))
+    val single = (m: Alphabet) => Cost(0, m) //0 cost if no multiplication
+    val mult = (c1: Answer, c2: Answer) => Cost(c1.v + c2.v + c1.m.x*c2.m.x*c2.m.y, Matrix(c1.m.x, c2.m.y))
   }
 
-  println("hello")
-  object mm extends MatrixGrammar
-  println("bye "+mm)
-  //val res = mm.el((0,1) )
+  class MatrixTest extends MatrixGrammar with MatrixAlgebra{
+    def input = Array(Matrix(1,2),Matrix(2,3),Matrix(3,4)) //define the input or the alphabet translations?
+  }
+
+  val mm = new MatrixTest()
+  val mats = Array((3,2),(2,4),(4,2),(2,5))
+  val res = mm.Parser.apply[Matrix](t => List(Matrix(t._1, t._2)))((1,1))
   //val res = mm.Parser(x => List(Matrix(x._1, x._2)))
-  //println(res)
+  println(res)
 }
